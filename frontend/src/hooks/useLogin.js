e3f6b130cd342bb0eb3060105cfff3df4317a1ab
Ten-T-Tonmoy <stu01754@gmail.com>
2025-05-02 21:39:09 +0600

    custom hooks for login and logout

---
+++ frontend/src/hooks/useLogin.js
@@ -0,0 +1,54 @@
+import toast from "react-hot-toast";
+
+import { useAuthContext } from "../context/authContext";
+import { useState } from "react";
+import axios from "axios";
+
+const useLogin = () => {
+  const [loading, setLoading] = useState(false);
+  const { setAuthUser } = useAuthContext();
+
+  //take a lesson u dont need to make whole shit async
+  const login = async (username, password) => {
+    const allGood = allOk(username, password);
+    if (!allGood) return;
+    setLoading(true);
+
+    try {
+      const res = await axios.post(
+        "/api/auth/login",
+        {
+          username,
+          password,
+        },
+        {
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+      if (res.data?.error) throw new Error(res.data.error);
+
+      localStorage.setItem("chatter", JSON.stringify(res.data));
+      setAuthUser(res);
+    } catch (e) {
+      toast.error(e.message);
+    } finally {
+      setLoading(false);
+      //well its weird but ahem
+    }
+  };
+
+  return { loading, login };
+};
+
+export default useLogin;
+
+function allOk(username, password) {
+  if (!username || !password) {
+    toast.error("All fields must be filled properly!");
+    return false;
+  }
+
+  return true;
+}
---
+++ frontend/src/hooks/useLogout.js
@@ -0,0 +1,31 @@
+import { useState } from "react";
+import { useAuthContext } from "../context/authContext";
+import toast from "react-hot-toast";
+import axios from "axios";
+
+const useLogout = () => {
+  const [loading, setLoading] = useState(false);
+  const { setAuthUser } = useAuthContext();
+
+  const logout = async () => {
+    setLoading(true);
+    try {
+      //ok post req doesnt necesessarily always need to have a body
+      const res = await axios.post("/api/auth/logout");
+      if (res.data?.error) {
+        throw new Error(res.data.error);
+      }
+
+      localStorage.removeItem("chatter");
+      setAuthUser(null);
+    } catch (e) {
+      toast.error(e.message);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return { loading, logout };
+};
+
+export default useLogout;
